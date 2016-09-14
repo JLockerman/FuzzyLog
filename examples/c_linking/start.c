eeee42bb665dfaa39daf49b0c20f57fde0bf1224
J <joshua.lockerman@yale.edu>
2016-09-14 18:19:11 -0400

    c_linking example now actually calls fuzzy_log code. Adds new_dir command to c_linking makefile. This command, used as 'make new_dir DIR_NAME=<new directory name>', creates a copy of the c_linking example in directory <new directory name> with paths configured to be suitable for use there. NOTE: do not call this with <new directory name> a sub dir of c_linking.

--- examples/c_linking/start.c
+++ examples/c_linking/start.c
@@ -1,6 +1,15 @@
+#include "stdio.h"
+
 #include "fuzzy_log.h"
+
+uint8_t callback(const uint8_t * _arg1, uint16_t _arg2)
+{
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
+	struct FuzzyLog* log = fuzzy_log_new(0, 3666, 0, 0, callback);
+	printf("fuzzy log client at %p\n", log);
 	return 0;
 }
