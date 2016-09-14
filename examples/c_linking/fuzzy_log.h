14115ba88a6e9f151e3bc0492481369237a407c5
J <joshua.lockerman@yale.edu>
2016-09-14 14:12:28 -0400

    c-linking example now compiles.

--- examples/c_linking/fuzzy_log.h
+++ examples/c_linking/fuzzy_log.h
@@ -3,26 +3,26 @@
 
 #include <stdint.h>
 
-extern struct FuzzyLog;
+struct FuzzyLog;
 
 typedef struct ChainAndEntry {
 	uint32_t chain;
 	uint32_t entry;
 } ChainAndEntry;
 
-typedef uint8_t (*fuzzy_log_callback)(const uint8_t *, u16);
+typedef uint8_t (*fuzzy_log_callback)(const uint8_t *, uint16_t);
 
-FuzzyLog *fuzzy_log_new(uint32_t server_ip_addr, uint16_t server_port,
+struct FuzzyLog *fuzzy_log_new(uint32_t server_ip_addr, uint16_t server_port,
 		const uint32_t *relevent_chains, uint16_t num_relevent_chains, fuzzy_log_callback callback);
 
-ChainAndEntry fuzzy_log_append(FuzzyLog *log, uint32_t chain,
+ChainAndEntry fuzzy_log_append(struct FuzzyLog *log, uint32_t chain,
 		const uint8_t *val, uint16_t len,
 		const ChainAndEntry* deps, uint16_t num_deps);
 
-void fuzzy_log_multiappend(FuzzyLog *log, uint32_t *chain, uint16_t num_chains,
+void fuzzy_log_multiappend(struct FuzzyLog *log, uint32_t *chain, uint16_t num_chains,
 		const uint8_t *val, uint16_t len,
 		const ChainAndEntry* deps, uint16_t num_deps);
 
-ChainAndEntry fuzzy_log_play_forward(FuzzyLog *log, uint32_t *chain);
+ChainAndEntry fuzzy_log_play_forward(struct FuzzyLog *log, uint32_t *chain);
 
 #endif
