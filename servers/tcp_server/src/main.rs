aea4e5cb282e0410229267fc507ca34885531a31
J <joshua.lockerman@yale.edu>
2016-10-19 15:13:21 -0400

    Adapt tcp server to allow starting a server group.

--- servers/tcp_server/src/main.rs
+++ servers/tcp_server/src/main.rs
@@ -12,16 +12,27 @@ use fuzzy_log::servers::tcp::Server;
 
 use mio::prelude::*;
 
-const ADDR_STR: &'static str = "0.0.0.0:13265";
-
 pub fn main() {
     let _ = env_logger::init();
-    let addr = match env::args().skip(1).next() {
-        Some(ref s) => ("0.0.0.0:".to_owned() + &*s).parse().expect("invalid inet address"),
-        _ => ADDR_STR.parse().expect("invalid default inet address"),
+    if env::args().len() < 2 {
+        println!("usage\n\tdelos_tcp_server <port> [<server num> <total servers>]");
+        return
+    }
+    let mut args = env::args().skip(1);
+    let port_num = args.next().unwrap();
+    let addr = ("0.0.0.0:".to_owned() + &port_num).parse()
+        .expect("Invalid port number.");
+    let (server_num, total_servers) = if let Some(n) = args.next() {
+        let n = n.parse().expect("Invalid server number.");
+        let t = args.next()
+            .expect("If a server number is given the total number of servers is also required.")
+            .parse().expect("Invalid total servers.");
+        (n, t)
+    } else {
+        (0, 1)
     };
     let mut event_loop = EventLoop::new().unwrap();
-    let mut server = Server::new(&addr, 0, 1, &mut event_loop).unwrap();
+    let mut server = Server::new(&addr, server_num, total_servers, &mut event_loop).unwrap();
     trace!("starting server");
     let _ = event_loop.run(&mut server);
 }
